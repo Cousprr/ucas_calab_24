438747b4c368ecadcc070a7bf54ec9569a40945d
Cousprr <cousprr2357@163.com>
2025-03-02 18:39:56 +0800

    init: prj2_exp7

---
+++ prj2_exp7/mycpu_env/myCPU/EXE.v
@@ -0,0 +1,74 @@
+module EXE(
+    input  wire        clk,
+    input  wire        resetn,
+    //from ID
+    output wire EXE_allow_in,
+    input wire ID_to_EXE_valid,
+    input wire [179:0] ID_to_EXE_bus,
+    //to MEM
+    output wire EXE_to_MEM_valid,
+    input wire MEM_allow_in,
+    output wire [102:0] EXE_to_MEM_bus,
+    // to data sram interface
+    output wire        data_sram_en,
+    output wire [3 :0] data_sram_we,
+    output wire [31:0] data_sram_addr,
+    output wire [31:0] data_sram_wdata
+    );
+    //inside EXE
+    reg EXE_valid;
+    wire EXE_ready_go;
+    assign EXE_ready_go = 1'b1;
+
+    wire [31:0] EXE_inst;
+    wire [31:0] EXE_pc;
+    assign EXE_to_MEM_valid = EXE_ready_go & EXE_valid;
+    assign EXE_allow_in = EXE_ready_go & MEM_allow_in | ~EXE_valid;
+
+    always @(posedge clk)begin
+        if(~resetn)begin
+            EXE_valid <= 1'b0;
+        end
+        else if(EXE_allow_in)begin
+            EXE_valid <= ID_to_EXE_valid;
+        end
+    end  
+
+    reg [179:0] ID_to_EXE_bus_valid;
+    always @(posedge clk)begin
+        if(ID_to_EXE_valid & EXE_allow_in)begin
+            ID_to_EXE_bus_valid <= ID_to_EXE_bus;
+        end
+    end
+    //bus
+    wire    [11:0]  alu_op;
+    wire    [31:0]  alu_src1;
+    wire    [31:0]  alu_src2;
+    wire            res_from_mem;
+    wire            gr_we;
+    wire            mem_we;
+    wire    [ 4:0]  dest;
+    wire    [31:0]  rkd_value;
+    
+    assign{alu_op, alu_src1, alu_src2, 
+           res_from_mem, gr_we, mem_we, dest,
+           rkd_value,
+           EXE_pc, EXE_inst} = ID_to_EXE_bus_valid;
+   
+    wire [31:0] alu_result;
+    alu u_alu(
+     .alu_op     (alu_op    ),
+     .alu_src1   (alu_src1  ),
+     .alu_src2   (alu_src2  ),
+     .alu_result (alu_result)
+     ); 
+    assign data_sram_en = 1'b1;
+    assign data_sram_we    = {4{mem_we}};
+    assign data_sram_addr  = alu_result;
+    assign data_sram_wdata = rkd_value;
+    
+    assign EXE_to_MEM_bus ={alu_result,
+                           res_from_mem, gr_we, dest, 
+                           EXE_pc, EXE_inst};
+
+endmodule
---
+++ prj2_exp7/mycpu_env/myCPU/IF.v
@@ -0,0 +1,63 @@
+module IF(
+    input  wire        clk,
+    input  wire        resetn,
+    //to ID
+    input  wire        ID_allow_in,//ID正常读入数据
+    output wire        IF_to_ID_valid,//IF正常工作，输出正确结果
+    output wire [63:0] IF_to_ID_bus,//pc & instruction
+    input  wire [32:0] ID_to_IF_bus,//br_taken & br_target
+    
+    //to 指令寄存器
+    output wire        inst_sram_en,
+    output wire [ 3:0] inst_sram_we,
+    output wire [31:0] inst_sram_addr,
+    output wire [31:0] inst_sram_wdata,
+    input  wire [31:0] inst_sram_rdata
+    );
+    
+    //inside IF
+    reg IF_valid;//IF级流水线存放数据有效
+    wire IF_allow_in;//IF正常读入数据
+    wire IF_ready_go;//IF工作有效，可进入下个阶段
+    
+    //for pc
+    reg [31:0] IF_pc;
+    wire [31:0] IF_nextpc;
+    wire [31:0] IF_seq_pc;
+    //for instruction
+    wire [31:0] IF_inst;
+    //if branch
+    wire br_taken;
+    wire [31:0] br_target;
+    
+    assign IF_ready_go = 1'b1;
+    assign IF_allow_in = IF_ready_go & ID_allow_in | ~IF_valid; //reset 或者 当前指令可以去ID
+    assign IF_to_ID_valid = IF_ready_go & IF_valid;
+    always @(posedge clk) begin
+        IF_valid <= resetn;
+    end
+  
+    assign IF_to_ID_bus = {IF_pc , IF_inst};
+    
+    assign {br_taken , br_target} = ID_to_IF_bus;
+    assign IF_seq_pc = IF_pc + 3'h4;
+    assign IF_nextpc = br_taken ? br_target : IF_seq_pc;
+
+    always @(posedge clk) begin
+        if (~resetn) begin
+            IF_pc <= 32'h1bfffffc;     //trick: to make nextpc be 0x1c000000 during reset 
+        end
+        //else begin
+        else if(IF_allow_in)begin
+            IF_pc <= IF_nextpc;
+        end
+    end
+    
+    //instruction
+    assign inst_sram_en = IF_allow_in;
+    assign  inst_sram_addr = IF_nextpc;
+    assign  inst_sram_we = 4'b0;
+    assign  inst_sram_wdata = 32'b0;
+    assign  IF_inst = inst_sram_rdata;
+    
+endmodule
