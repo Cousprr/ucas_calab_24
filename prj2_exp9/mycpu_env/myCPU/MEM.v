4d3798b6f2c33df09391c757a5142a6a91e68585
Cousprr <cousprr2357@163.com>
2025-03-02 18:48:05 +0800

    init: prj2_exp9

---
+++ prj2_exp9/mycpu_env/myCPU/MEM.v
@@ -0,0 +1,61 @@
+module MEM(
+    input  wire        clk,
+    input  wire        resetn,
+    //from EXE
+    output wire MEM_allow_in,
+    input wire EXE_to_MEM_valid,
+    input wire [102:0] EXE_to_MEM_bus,
+    //to WB
+    output wire MEM_to_WB_valid,
+    input wire WB_allow_in,
+    output wire [101:0] MEM_to_WB_bus,
+    //to data sram interface
+    input  wire [31:0] data_sram_rdata,
+    //conflict
+    output wire [37:0] MEM_wr_bus
+);
+    //inside MEM
+    reg MEM_valid;
+    wire MEM_ready_go;
+    assign MEM_ready_go = 1'b1;
+    
+    wire [31:0] MEM_inst;
+    wire [31:0] MEM_pc;
+    assign MEM_to_WB_valid = MEM_ready_go & MEM_valid;
+    assign MEM_allow_in = MEM_ready_go & WB_allow_in | ~MEM_valid;
+    always @(posedge clk)begin
+        if(~resetn)begin
+            MEM_valid <= 1'b0;
+        end
+        else if(MEM_allow_in)begin
+            MEM_valid <= EXE_to_MEM_valid;
+        end
+    end  
+
+    reg [102:0] EXE_to_MEM_bus_valid;
+    always @(posedge clk)begin
+        if(EXE_to_MEM_valid & MEM_allow_in)begin
+            EXE_to_MEM_bus_valid <= EXE_to_MEM_bus;
+        end
+    end
+    
+    //bus
+    wire    [ 31:0] alu_result;
+    wire            res_from_mem;
+    wire            gr_we;
+    wire    [  4:0] dest;
+    assign {alu_result,
+           res_from_mem, gr_we, dest, 
+           MEM_pc, MEM_inst} = EXE_to_MEM_bus_valid;
+           
+    wire [31:0] final_result;
+    wire [31:0] mem_result;
+    assign mem_result   = data_sram_rdata;
+    assign final_result = res_from_mem ? mem_result : alu_result;
+    
+    //bus
+    assign MEM_to_WB_bus = {final_result,
+                            gr_we, dest,
+                            MEM_pc, MEM_inst};
+    assign MEM_wr_bus = {gr_we & MEM_valid, dest, final_result};
+endmodule
---
+++ prj2_exp9/mycpu_env/myCPU/mycpu_top.v
@@ -0,0 +1,114 @@
+module mycpu_top(
+    input  wire        clk,
+    input  wire        resetn,
+    // inst sram interface
+    output wire        inst_sram_en,
+    output wire [ 3:0] inst_sram_we,
+    output wire [31:0] inst_sram_addr,
+    output wire [31:0] inst_sram_wdata,
+    input  wire [31:0] inst_sram_rdata,
+    // data sram interface
+    output wire        data_sram_en,
+    output wire [ 3:0] data_sram_we,
+    output wire [31:0] data_sram_addr,
+    output wire [31:0] data_sram_wdata,
+    input  wire [31:0] data_sram_rdata,
+    // trace debug interface
+    output wire [31:0] debug_wb_pc,
+    output wire [ 3:0] debug_wb_rf_we,
+    output wire [ 4:0] debug_wb_rf_wnum,
+    output wire [31:0] debug_wb_rf_wdata
+);
+
+    wire ID_allow_in;
+    wire MEM_allow_in;
+    wire WB_allow_in;
+    wire IF_valid;
+    wire IF_to_ID_valid;
+    wire ID_to_EXE_valid;
+    wire EXE_to_MEM_valid;
+    wire MEM_to_WB_valid;
+    wire [63:0] IF_to_ID_bus;
+    wire [32:0] ID_to_IF_bus;
+
+    wire [179:0] ID_to_EXE_bus;
+    wire [102:0] EXE_to_MEM_bus;
+    wire [101:0] MEM_to_WB_bus;
+    wire [37:0] WB_to_ID_bus;
+    wire [38:0] EXE_wr_bus;
+    wire [37:0] MEM_wr_bus;
+
+
+    
+    IF u_IF(
+    .clk(clk),
+    .resetn(resetn),
+    .ID_allow_in(ID_allow_in),
+    .IF_to_ID_valid(IF_to_ID_valid),
+    .IF_to_ID_bus(IF_to_ID_bus),
+    .ID_to_IF_bus(ID_to_IF_bus),
+    .inst_sram_en(inst_sram_en),
+    .inst_sram_we(inst_sram_we),
+    .inst_sram_addr(inst_sram_addr),
+    .inst_sram_wdata(inst_sram_wdata),
+    .inst_sram_rdata(inst_sram_rdata)
+    );
+ 
+    ID u_ID(
+    .clk(clk),
+    .resetn(resetn),
+    .IF_to_ID_valid(IF_to_ID_valid),
+    .ID_allow_in(ID_allow_in),
+    .IF_to_ID_bus(IF_to_ID_bus),
+    .ID_to_IF_bus(ID_to_IF_bus),
+    .EXE_allow_in(EXE_allow_in),
+    .ID_to_EXE_valid(ID_to_EXE_valid),
+    .ID_to_EXE_bus(ID_to_EXE_bus),
+    .WB_to_ID_bus(WB_to_ID_bus),
+    .EXE_wr_bus(EXE_wr_bus),
+    .MEM_wr_bus(MEM_wr_bus)
+    );
+    
+    EXE u_EXE(
+    .clk(clk),
+    .resetn(resetn),
+    .EXE_allow_in(EXE_allow_in),
+    .ID_to_EXE_valid(ID_to_EXE_valid),
+    .ID_to_EXE_bus(ID_to_EXE_bus),
+    .EXE_to_MEM_valid(EXE_to_MEM_valid),
+    .MEM_allow_in(MEM_allow_in),
+    .EXE_to_MEM_bus(EXE_to_MEM_bus),
+    .data_sram_en(data_sram_en),
+    .data_sram_we(data_sram_we),
+    .data_sram_addr(data_sram_addr),
+    .data_sram_wdata(data_sram_wdata),
+    .EXE_wr_bus(EXE_wr_bus)
+    );
+    
+    MEM u_MEM(
+    .clk(clk),
+    .resetn(resetn),
+    .MEM_allow_in(MEM_allow_in),
+    .EXE_to_MEM_valid(EXE_to_MEM_valid),
+    .EXE_to_MEM_bus(EXE_to_MEM_bus),
+    .MEM_to_WB_valid(MEM_to_WB_valid),
+    .WB_allow_in(WB_allow_in),
+    .MEM_to_WB_bus(MEM_to_WB_bus),
+    .data_sram_rdata(data_sram_rdata),
+    .MEM_wr_bus(MEM_wr_bus)
+    );
+
+    WB u_WB(
+    .clk(clk),
+    .resetn(resetn),
+    .WB_allow_in(WB_allow_in),
+    .MEM_to_WB_valid(MEM_to_WB_valid),
+    .MEM_to_WB_bus(MEM_to_WB_bus),
+    .WB_to_ID_bus(WB_to_ID_bus),
+    .debug_wb_pc(debug_wb_pc),
+    .debug_wb_rf_we(debug_wb_rf_we),
+    .debug_wb_rf_wnum(debug_wb_rf_wnum),
+    .debug_wb_rf_wdata(debug_wb_rf_wdata)
+    );
+
+endmodule
